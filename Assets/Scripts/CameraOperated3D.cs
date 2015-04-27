10689ee2675a63b31f4dbc65dea2d6b207995177
GeXiaoDong <421536265@qq.com>
2015-04-27 20:07:39 +0800

    GXD

--- Assets/Scripts/CameraOperated3D.cs
+++ Assets/Scripts/CameraOperated3D.cs
@@ -6,7 +6,10 @@ public class CameraOperated3D : MonoBehaviour {
     public GameObject target;
     private Vector3 targetVector3 = Vector3.zero;
 
-    private float distance;
+    private float normalDistance = 60f;
+    private float MouseZoomMin = 20f;
+    private float MouseZoomMax = 100f;
+    private float MouseWheelSensitivity = 15f;
 
     private const float X_SPEED = 250.0f;
     private const float Y_SPEED = 120.0f;
@@ -14,6 +17,7 @@ public class CameraOperated3D : MonoBehaviour {
     private float y = 0.0f;
     private int yMinLimit = -45;
     private int yMaxLimit = 45;
+    private float distance;
 
     public void Start()
     {
@@ -24,22 +28,63 @@ public class CameraOperated3D : MonoBehaviour {
         Vector3 rotateVector3 = transform.rotation.eulerAngles;
         x = rotateVector3.y;
         y = rotateVector3.x;
-        transform.rotation = Quaternion.Euler(y, x, 0);
-        transform.position = transform.rotation * new Vector3(0.0f, 0.0f, -distance) + targetVector3;
+        RotatedCamera(y, x, 0, distance);
+
+        normalDistance = transform.camera.fieldOfView;
     }
 
+    private float axisX;
+    private float flag = 1f;
+    private float gradualSpeed = 2f;
+    private Vector2 gradualVector2;
     public void Update()
     {
+        if (flag < 1f)
+        {
+            flag += Time.deltaTime * gradualSpeed;
+            gradualVector2 = Vector2.Lerp(gradualVector2, Vector2.zero, flag);
+            x += gradualVector2.x;
+            RotatedCamera(y, x, 0, distance);
+        }
+
+        // 旋转
         if (Input.GetMouseButton(1))
         {
-            x += (Input.GetAxis("Mouse X") * X_SPEED * 0.02f);
+            axisX = (Input.GetAxis("Mouse X") * X_SPEED * 0.02f);
+            x += axisX;
             y -= (Input.GetAxis("Mouse Y") * Y_SPEED * 0.02f);
 
             y = ClampAngle(y, yMinLimit, yMaxLimit);
 
-            transform.rotation = Quaternion.Euler(y, x, 0);
-            transform.position = transform.rotation * new Vector3(0.0f, 0.0f, -distance) + targetVector3;
+            RotatedCamera(y, x, 0, distance);
+
+            if (Mathf.Abs(axisX) > 10f)
+            {
+                flag = 0f;
+                gradualVector2 = new Vector2(axisX, 0f);
+            }
         }
+        // 放大与缩小
+        else if(Input.GetAxis("Mouse ScrollWheel")!=0)
+        {
+            normalDistance -= Input.GetAxis("Mouse ScrollWheel") * MouseWheelSensitivity;
+            normalDistance = Mathf.Clamp(normalDistance, MouseZoomMin, MouseZoomMax);
+
+            transform.camera.fieldOfView = normalDistance;
+        }
+
+    }
+
+    private Vector2 axisVector2;
+    private void RotatedCameraGradual(float axisX, float axisY)
+    {
+        axisVector2 = new Vector2(axisX, axisY);
+    }
+
+    private void RotatedCamera(float x, float y, float z, float distance)
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(x, y, z));
+        transform.position = transform.rotation * new Vector3(0.0f, 0.0f, -distance) + targetVector3;
     }
 
     static float ClampAngle(float angle, float min, float max)
