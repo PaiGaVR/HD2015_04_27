35763adb64de442bf1d0bbf394089c9b6a8bdda4
Hy490950439 <490950439@qq.com>
2015-04-29 16:14:12 +0800

    HY

--- Assets/Scripts/Disperse.cs
+++ Assets/Scripts/Disperse.cs
@@ -1,42 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Disperse : MonoBehaviour
 {
     private GameObject gameO;
-	// Use this for initialization
-	void Start ()
-    {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-       
-	}
+    private int count = 0;
 
-   void MouseDoubelCheck()
+    void Start()
     {
-        foreach (Transform trans in gameO.transform)
+        foreach (Transform trans in transform.parent)
         {
-            if (trans.localPosition.y < 0 && trans.localPosition.z < 0)
+            if (trans.name == "GameObject")
             {
-                trans.localPosition = new Vector3(Mathf.Lerp(trans.localPosition.x, -1f, Time.time), Mathf.Lerp(trans.localPosition.y, trans.localPosition.y * 2f, Time.time), Mathf.Lerp(trans.localPosition.z, trans.localPosition.y * 2f, Time.time));
+                gameO = trans.gameObject;
             }
-            if (trans.localPosition.y < 0 && trans.localPosition.z > 0)
-            {
-                trans.localPosition = new Vector3(Mathf.Lerp(trans.localPosition.x, 1f, Time.time), Mathf.Lerp(trans.localPosition.y, trans.localPosition.y * 2f, Time.time), Mathf.Lerp(trans.localPosition.z, trans.localPosition.y * 2f, Time.time));
-            }
+        }
+        if (gameO != null)
+        {
+            gameO.GetComponent<DispersionController>().Dispersion += Dis;
+            gameO.GetComponent<DispersionController>().Aggregation += Agg;
+        }
+    }
 
-            if (trans.localPosition.y > 0 && trans.localPosition.z > 0)
-            {
-                trans.localPosition = new Vector3(Mathf.Lerp(trans.localPosition.x, 1f, Time.time), Mathf.Lerp(trans.localPosition.y, trans.localPosition.y * 2f, Time.time), Mathf.Lerp(trans.localPosition.z, trans.localPosition.y * 2f, Time.time));
-            }
-            if (trans.localPosition.y > 0 && trans.localPosition.z < 0)
-            {
-                trans.localPosition = new Vector3(Mathf.Lerp(trans.localPosition.x, -1f, Time.time), Mathf.Lerp(trans.localPosition.y, trans.localPosition.y * 2f, Time.time), Mathf.Lerp(trans.localPosition.z, trans.localPosition.y * 2f, Time.time));
-            }
+    void Dis(GameObject ga)
+    {
+        foreach (Transform trans in ga.transform)
+        {
+            count++;
+            trans.localPosition = new Vector3(Mathf.Lerp(trans.localPosition.x, (-1) * count, Time.time),
+                Mathf.Lerp(trans.localPosition.y, trans.localPosition.y * 2f, Time.time),
+                Mathf.Lerp(trans.localPosition.z, trans.localPosition.y * 2f, Time.time));
+        }
+        count = 0;
+    }
+
+    void Agg(GameObject ga)
+    {
+        foreach (Transform trans in ga.transform)
+        {
+            trans.localPosition = new Vector3(Mathf.Lerp(trans.localPosition.x, 0, Time.time),
+                Mathf.Lerp(trans.localPosition.y, trans.localPosition.y / 2f, Time.time),
+                Mathf.Lerp(trans.localPosition.z, trans.localPosition.y / 2f, Time.time));
         }
     }
 }
+
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour {
+    private bool isDoubleClick = false;
+    private bool isDispersion = false;
+    private int count = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isDoubleClick)
+        {
+            if (!isDispersion)
+            {
+                foreach (Transform trans in transform)
+                {
+                    count++;
+                    Debug.Log("before" + trans.localPosition);
+                    trans.localPosition = new Vector3(Mathf.Lerp(trans.localPosition.x, (float)Math.Pow(-1,count), Time.time),
+                        Mathf.Lerp(trans.localPosition.y, trans.localPosition.y * 2f , Time.time),
+                        Mathf.Lerp(trans.localPosition.z, trans.localPosition.y * 2f , Time.time));
+                    Debug.Log("later" + trans.localPosition);
+                }
+                count = 0;
+
+                isDispersion = true;
+            }
+            else
+            {
+                foreach (Transform trans in transform)
+                {
+                    trans.localPosition = new Vector3(Mathf.Lerp(trans.localPosition.x, 0, Time.time),
+                        Mathf.Lerp(trans.localPosition.y, trans.localPosition.y / 2f, Time.time),
+                        Mathf.Lerp(trans.localPosition.z, trans.localPosition.y / 2f, Time.time));
+                }
+                isDispersion = false;
+            }
+
+            isDoubleClick = false;
+        }
+	}
+
+    void OnGUI()
+    {
+        Event Mouse = Event.current;
+        if (Mouse.isMouse && Mouse.type == EventType.MouseDown && Mouse.clickCount == 2)
+        {
+            isDoubleClick = true;
+        }
+    }
+}
